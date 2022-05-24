d947f489039d39a1f1aaabe61b0f9963a02c56a0
SamuelDAlencar <sarl.2001@hotmail.com>
2022-05-24 11:14:50 -0300

    feat: requirements 1 and 11 done

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true }
+  }, { timestamps: false });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'id'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'id'
+    });
+  };
+
+  return PostCategory;
+};
