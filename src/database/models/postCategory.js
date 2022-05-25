06fad6fffe0df7e6d13d911f86d4bf7dd1d62820
SamuelDAlencar <sarl.2001@hotmail.com>
2022-05-25 20:09:18 -0300

    fix: returned the id attributes to all the models that had them removed in the last requirement

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,6 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
-  const PostCategory = sequelize.define('PostCategory',
-    {},
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true }
+  },
     { timestamps: false });
 
   PostCategory.associate = (models) => {
